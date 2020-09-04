2b6cf6a5938ff9e0685176709da569120d4385d9
CStank89 <C.Stank89@gmail.com>
2020-09-04 12:37:54 -0400

    for some reason im getting object is undefined and I cant figure out why i also started the foreach on task 2 but i struggle with for each and just irations over arrays or object

--- components/Cards.js
+++ components/Cards.js
@@ -1,3 +1,10 @@
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+    .then (stuff =>{
+        console.log(stuff)
+    })
+    .catch (error =>{
+        console.log(error)
+    })
 // STEP 3: Create article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-api.herokuapp.com/articles
@@ -9,14 +16,53 @@
 //
 // <div class="card">
 //   <div class="headline">{Headline of article}</div>
-//   <div class="author">
-//     <div class="img-container">
-//       <img src={url of authors image} />
-//     </div>
-//     <span>By {author's name}</span>
-//   </div>
+
+//       <div class="author">
+
+//       <div class="img-container">
+//           <img src={url of authors image} />
+//       </div>
+//       <span>By {author's name}</span>
+
+//      </div>
 // </div>
 //
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+function cardMaker(object){
+    const card = document.createElement('div')
+    card.classList= ('card')
+
+    const headLine = document.createElement('div')
+    headLine.classList=('headline')
+
+    const author = document.createElement('div')
+    author.classList = ('author')
+
+    const imgDiv = document.createElement('div')
+    imgDiv.classList = ('img-container')
+
+    const img = document.createElement('img')
+    img.src = obj.authoPhoto
+
+    const span = document.createElement('span')
+    span.classList = `By: ${authorName}`
+
+    card.appendChild(headLine)
+    headLine.appendChild(author)
+    headLine.appendChild(imgDiv)
+    imgDiv.appendChild(img)
+    span.appendChild(headLine)
+
+    card.addEventListener('click', event =>{
+        console.log(headLine)
+    })
+
+    return card
+}
+    const newCard = document.querySelector('.cards-container')
+    const card = cardMaker()
+    newCard.appendChild(card)
--- components/Tabs.js
+++ components/Tabs.js
@@ -1,3 +1,4 @@
+
 // STEP 2: Create tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-api.herokuapp.com/topics
@@ -9,3 +10,23 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+axios.get('https://lambda-times-api.herokuapp.com/topics')
+    .then(stuff=>{
+        topic.forEach(element =>{
+            element.createElement('div')
+            element.classList.add('tab')
+            element.textContent = object.topic
+         }) 
+      })
+    .catch(error =>{
+        console.log(error)
+    })
+
+
+
+//  topic.forEach(element =>{
+//     element.createElement('div')
+//     element.classList.add('tab')
+//     element.textContent = object.topic
+//  }) 
